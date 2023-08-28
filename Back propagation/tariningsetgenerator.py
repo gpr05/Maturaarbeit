29125e3057e202734ac0aeda3ef1ad5b76cf2583
gpr05 <87725635+gpr05@users.noreply.github.com>
2023-08-28 21:50:40 +0200

    some changes

--- Back propagation/tariningsetgenerator.py
+++ Back propagation/tariningsetgenerator.py
@@ -7,8 +7,8 @@ import os
 b=np.array([0.95764,0.45682,0.52884,0.4,0.566,1.23])
 a=np.array([1.09934,0.9454,0.345,0.8993,0.3452,0.0345,1.236,0.4444,0.555])
 rgb=[]
-B=np.random.rand(8,1)
-W=np.random.rand(18,1)
+B=np.random.rand(5,1)#(8,1)
+W=np.random.rand(15,1)#(18,1)
 print(B,W)
 
 def evalnn(rgb):
@@ -37,15 +37,50 @@ def evalnnWB(rgb):
          f(f(rgb[0]*W[0]+B[0])*W[9]+f(rgb[1]*W[1]+B[1])*W[10]+f(rgb[2]*W[2]+B[2])*W[11]+B[5])*W[17]+B[7])
     return rgb,re,li
 
-def eval():
-    for i in range(100):
+def H0pre(inp):
+    return inp[0]*W[0]+inp[1]*W[1]+inp[2]*W[2]+B[0]
+
+def H0(inp):
+    return f(H0pre(inp))
+
+def H1pre(inp):
+    return inp[0]*W[3]+inp[1]*W[4]+inp[2]*W[5]+B[1]
+
+def H1(inp):
+    return f(H1pre(inp))
+
+def H2pre(inp):
+    return inp[0]*W[6]+inp[1]*W[7]+inp[2]*W[8]+B[2]
+
+def H2(inp):
+    return f(H2pre(inp))
+
+def O0pre(inp):
+    return H0(inp)*W[9]+H1(inp)*W[10]+H2(inp)*W[11]+B[3]
+
+def O0(inp):
+    return f(O0pre(inp))
+
+def O1pre(inp):
+    return H0(inp)*W[12]+H1(inp)*W[13]+H2(inp)*W[14]+B[4]
+
+def O1(inp):
+    return f(O1pre(inp))
+
+def evalnn3(inp):
+    re=O0(inp)
+    li=O1(inp)
+    return inp,re,li
+
+def eval(k):
+    for i in range(k):
         d=[0.0,0.0,0.0]
         for r in range(3):
             d[r]=np.random.random()*255
-        rgb.append([evalnnWB(d)])
+        rgb.append([evalnn3(d)])
     df = pd.DataFrame([rgb] )#columns=["R", "G",'B','re','li'])  # doctest: +SKIP
     '''with pd.ExcelWriter("D:training sets.xlsx",mode='a',if_sheet_exists='overlay') as writer:
         df.to_excel(writer)  # doctest: +SKIP'''
-    for g in range(100):
+    for g in range(k):
         print(rgb[g])
-eval()
+eval(15)
